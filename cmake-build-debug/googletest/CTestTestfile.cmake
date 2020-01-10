056d7921dc54883a72bad95e3de9a2a7b316b8be
Pengfei Zhang <pfzhang@cse.cuhk.edu.hk>
2020-01-10 10:34:22 +0800

    rm cmake-build-debug

--- cmake-build-debug/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /tmp/tmp.8a1X64Q1z0/googletest
-# Build directory: /tmp/tmp.8a1X64Q1z0/cmake-build-debug/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /tmp/tmp.8a1X64Q1z0/googletest/googletest
-# Build directory: /tmp/tmp.8a1X64Q1z0/cmake-build-debug/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
