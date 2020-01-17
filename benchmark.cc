d1289ae078ea3c0413a70075a7a9c8748cce6209
Pengfei Zhang <pfzhang@cse.cuhk.edu.hk>
2020-01-17 20:07:19 +0800

    new benchmark.cc

--- benchmark.cc
+++ benchmark.cc
@@ -20,10 +20,10 @@ limitations under the License.
 #include "test.h"
 
 
-#define RUY_TEST_LHSSCALAR float
-#define RUY_TEST_RHSSCALAR float
-#define RUY_TEST_ACCUMSCALAR float
-#define RUY_TEST_DSTSCALAR float
+#define RUY_TEST_LHSSCALAR uint8_t
+#define RUY_TEST_RHSSCALAR uint8_t
+#define RUY_TEST_ACCUMSCALAR int32_t
+#define RUY_TEST_DSTSCALAR int32_t
 
 namespace ruy {
 
@@ -44,6 +44,16 @@ struct BenchmarkShape {
   int symm_rhs;
 };
 
+struct Shape {
+  int rows;
+  int depth;
+  int cols;
+  Shape(int _rows, int _depth, int _cols):
+  rows(_rows), depth(_depth), cols(_cols)
+  {
+  }
+};
+
 template <typename TestSetType>
 std::vector<std::unique_ptr<TestResult<DstScalar>>> BenchmarkRCC(
     const BenchmarkShape& shape) {
@@ -55,14 +65,14 @@ std::vector<std::unique_ptr<TestResult<DstScalar>>> BenchmarkRCC(
   test_set.rhs_order = Order::kColMajor; // It is col major
   test_set.dst_order = Order::kColMajor; // It is col major
   test_set.layout_style = LayoutStyle::kPackedLinear; // What is kPackedLinear.
-  test_set.benchmark = true;
-  const int asymmetry_lhs = shape.symm_lhs ? 0 : 1;
+  test_set.benchmark = true; // The benchmark is true here.
+  const int asymmetry_lhs = shape.symm_lhs ? 0 : 1; // If shape.symm_lhs is 0, then asymmetry_lhs is 0
   const int asymmetry_rhs = shape.symm_rhs ? 0 : 1;
   test_set.lhs_zero_point = SymmetricZeroPoint<LhsScalar>() + asymmetry_lhs;
   test_set.rhs_zero_point = SymmetricZeroPoint<RhsScalar>() + asymmetry_rhs;
   test_set.use_specified_zero_points = true; // In the benchmark, we use specified zero points by default.
   // Whether each channel uses a different quantization parameters.
-  test_set.perchannel = GetBoolEnvVarOrFalse("PERCHANNEL");
+  test_set.perchannel = GetBoolEnvVarOrFalse("PERCHANNEL"); // Define the per channel as environment variable.
   // Whether the left matrix is prepacked.
   test_set.benchmark_prepack_lhs = GetBoolEnvVarOrFalse("PREPACK_LHS");
   // Whether the right matrix is prepacked.
@@ -70,6 +80,8 @@ std::vector<std::unique_ptr<TestResult<DstScalar>>> BenchmarkRCC(
   test_set.Run();
   return std::move(test_set.results);
 }
+
+
 
 void Benchmark() {
 
@@ -88,12 +100,32 @@ void Benchmark() {
   static constexpr int cubic_size_multiplier = 8;
 
   // if benchmark_cubic == true.
+
+
   if (benchmark_cubic) {
 #ifdef _WIN32
     _putenv_s("QUICK_BENCHMARK", "1");
 #else
     setenv("QUICK_BENCHMARK", "1", 0);
 #endif
+
+    //td::map<>
+
+    std::vector<Shape> mobilenetv1;
+
+   // mobilenetv1.push_back(Shape(112 * 112, 32, 3 * 3 * 3));
+   // mobilenetv1.push_back(Shape(56 * 56, 128, 64));
+   // mobilenetv1.push_back(Shape(56 * 56, 128, 128));
+    mobilenetv1.push_back(Shape(28 * 28, 256, 128));
+   // mobilenetv1.push_back(Shape(28 * 28, 256, 256));
+   // mobilenetv1.push_back(Shape(14 * 14, 512, 256));
+   // mobilenetv1.push_back(Shape(14 * 14, 512, 512));
+   // mobilenetv1.push_back(Shape(7 * 7, 1024, 512));
+   // mobilenetv1.push_back(Shape(7 * 7, 1024, 1024));
+
+
+
+#if 0
     std::vector<int> sizes;
     for (int i = 2 * cubic_size_multiplier; i <= (512 * cubic_size_multiplier);
          i *= 2) {
@@ -102,11 +134,12 @@ void Benchmark() {
         sizes.push_back(i * 3 / 2);
       }
     }
-    for (int i : sizes) {
+#endif
+    for (auto & layer : mobilenetv1) {
       BenchmarkShape shape;
-      shape.rows = i;
-      shape.cols = i;
-      shape.depth = i;
+      shape.rows = layer.rows;
+      shape.cols = layer.cols;
+      shape.depth = layer.depth;
       shape.symm_lhs = symm_lhs;
       shape.symm_rhs = symm_rhs;
       shapes.push_back(shape);
@@ -131,6 +164,7 @@ void Benchmark() {
     const auto& shape = shapes[i];
     const auto& results = BenchmarkRCC<TestSetType>(shape);
     if (i == 0) {
+#if 0
       if (benchmark_cubic) {
         printf("size");
         for (const auto& result : results) {
@@ -141,12 +175,26 @@ void Benchmark() {
         printf("path,shape,Gop/s\n");
       }
       fflush(stdout);
+#endif
+      if(benchmark_cubic){
+          printf("Rows, Depth, Cols");
+
+          for(const auto& result: results){
+              printf(" ,%s", PathName(*result).c_str());
+          }
+          printf("\n");
+      } else {
+          printf("path,shape,Gop/s\n");
+      }
     }
+
     if (benchmark_cubic) {
-      printf("%d", shape.rows);
+
+      printf("%d, %d, %d", shape.rows, shape.depth, shape.cols);
       for (const auto& result : results) {
-        printf(",%.4g", 2.0e-9 * shape.rows * shape.cols * shape.depth /
-                            result->latency);
+        //printf(",%.4g", 2.0e-9 * shape.rows * shape.cols * shape.depth /
+        //                    result->latency);
+        printf(",%4g", 1.0e3 * result->latency);
         if (GetBoolEnvVarOrFalse("RUY_BENCHMARK_PMU")) {
           printf(",%.3g,%.3g,%.3g,%.3g,%.3g,%.3g,%.3g,%.3g",
                  result->l1_refill_rate, result->l2_refill_rate,
